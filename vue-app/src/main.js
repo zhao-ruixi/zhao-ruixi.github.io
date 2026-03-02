eb08ba9effa55a1bd2d85d958f7f46ad92062ddc
Zhao Kaijie <19370986818@163.com>
2026-03-03 00:45:35 +0800

    vibe commite

---
+++ vue-app/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.mount('#app')
